f9fe25ca0668f0057fc29a529c8baee06194e625
rryqszq4 <279664262@163.com>
2015-04-07 19:58:34 +0800

    [sort] insert sort

---
+++ sort/sort.cpp
@@ -0,0 +1,11 @@
+
+
+template<class T>
+void insertionsort(T data[], int n){
+	for (int i=1,j; i < n; i++){
+		T tmp = data[i];
+		for (j = i; j > 0 && tmp < data[j-1]; j--)
+			data[j] = data[j-1];
+		data[j] = tmp;
+	}
+}
