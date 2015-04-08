04924eea929b548d53a1b37d0344b8b94ac75562
rryqszq4 <279664262@163.com>
2015-04-08 14:41:57 +0800

    [sort] bubble sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -20,3 +20,13 @@ void selectionsort(T data[], int n){
 		swap(data[least], data[i]);
 	}
 }
+
+template<class T>
+void bubblesort(T data[], int n){
+	for (int i=0; i < n-1; i++){
+		for (int j = n-1; j > i; --j){
+			if (data[j] < data[j-1])
+				swap(data[j], data[j-1]);
+		}
+	}
+}
